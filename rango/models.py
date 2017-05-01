96963f20b81c7bdf20965f7181fec16444c0fdf1
Artem Suzdal <artemsuzdal@gmail.com>
2017-05-01 19:24:00 +0300

    Implemented User authentication functionality. Allowed adding new Categories and Page only logged in Users.

--- rango/models.py
+++ rango/models.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
@@ -36,3 +37,18 @@ class Page(models.Model):
 
     def __unicode__(self):
         return self.title  # for Python 2.7
+
+
+class UserProfile(models.Model):
+    # This line is required. Links UserProfile to a User model instance.
+    user = models.OneToOneField(User)
+
+    # The additional attributes we wish to include.
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+    def __unicode__(self):
+        return self.user.username # for Python 2.7
